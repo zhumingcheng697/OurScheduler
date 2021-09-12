a21a6a65519758229af43c5996114b357893b35e
McCoy Zhu <mz1945@nyu.edu>
2021-09-12 03:06:18 -0400

    ADD: retrieve and insert

---
+++ scraper/insert.js
@@ -0,0 +1,64 @@
+function dbInsert(schoolInfo, classInfo) {
+    const { MongoClient } = require("mongodb");
+    const url = "mongodb+srv://admin:hackersunion@myscheduler.wcsib.mongodb.net/Myscheduler_classes?retryWrites=true&w=majority";
+    const client = new MongoClient(url);
+    const dbName = "Classes";
+    const school = schoolInfo;
+    const lectures = [];
+    const extras = [];
+    const scraped = classInfo;
+    for (let i = 0; i < scraped.length; i++) {
+        var time_arr = scraped[i][4].split("-");
+
+        var start_time_nums = time_arr[0].slice(0, -2).split(":");
+        var start_time = parseInt(start_time_nums[0]) * 60 + parseInt(start_time_nums[1]);
+        var start_time_ampm = time_arr[0].slice(-2);
+        if (start_time_ampm == "pm") {
+            start_time += 720;
+        }
+
+        var end_time_nums = time_arr[1].slice(0, -2).split(":");
+        var end_time = parseInt(end_time_nums[0]) * 60 + parseInt(end_time_nums[1]);
+        var end_time_ampm = time_arr[1].slice(-2);
+        if (end_time_ampm == "pm") {
+            end_time += 720;
+        }
+
+        var temp = [scraped[i][2]];
+        for (let j = 0; j < scraped[i][3].length; j++) {
+            temp.push([start_time + scraped[i][3][j] * 1440, end_time + scraped[i][3][j] * 1440]);
+        }
+        if (scraped[i][5] == 0) {
+            extras.push(temp);
+        } else {
+            lectures.push(temp);
+        }
+    }
+
+    async function run() {
+        try {
+            await client.connect();
+            //console.log("Connected correctly to server");
+            const db = client.db(dbName);
+            const col = db.collection(school);
+            let classDocument = {
+                "name": scraped[0][0], //course name as given
+                "credits": scraped[0][5], //scrape credits
+                "label": scraped[0][1], //class id until '-'
+                "lectures": lectures, //lecture sections
+                "extras": extras //extra sections
+            };
+            const p = await col.insertOne(classDocument);
+            //const myDoc = await col.findOne();
+            //console.log(myDoc);
+        } catch (err) {
+            console.log(err.stack);
+        } finally {
+            await client.close();
+        }
+    }
+
+    run().catch(console.dir);
+}
+
+module.exports = dbInsert;
---
+++ scraper/retrieve.js
@@ -0,0 +1,27 @@
+async function dbRetrieve(schoolInfo, classInfo) {
+    const { MongoClient } = require("mongodb");
+    const url = "mongodb+srv://admin:hackersunion@myscheduler.wcsib.mongodb.net/Myscheduler_classes?retryWrites=true&w=majority";
+    const client = new MongoClient(url);
+    const dbName = "Classes";
+    const school = schoolInfo;
+
+    async function run() {
+        try {
+            await client.connect();
+            const db = client.db(dbName);
+            const col = db.collection(school);
+            const query = { $or: [{ name: classInfo }, { label: classInfo }] };
+            return await col.findOne(query);
+        } finally {
+            await client.close();
+        }
+    }
+
+    return await run();
+}
+
+module.exports = dbRetrieve;
+
+// (async () => {
+//     console.log(require("util").inspect(await dbretrieve("nyu", "CSUY 2214"), false, null));
+// })();
