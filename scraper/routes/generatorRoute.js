17800822c3e3a1bfe1e282f7ad0cfbae082cc739
McCoy Zhu <mz1945@nyu.edu>
2021-09-12 06:39:03 -0400

    ADD: generatorRoute

---
+++ scraper/routes/generatorRoute.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const generator = require("../../algo/generator");
+
+router.get("/", (req, res) => {
+    const target = req.query && req.query.prop;
+    const prop = JSON.parse(decodeURI(req.query.prop))
+
+    generator(prop).then((info) => {
+        console.log(info)
+        return res.send(info);
+    }).catch((e) => {
+        return res.send(req.query && req.query.dev === "true" ? e : null);
+    });
+});
+
+module.exports = router;
