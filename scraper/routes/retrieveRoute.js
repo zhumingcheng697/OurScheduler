5c96c20aef5e868a0d4a1f40c8958d9f519fbadf
McCoy Zhu <mz1945@nyu.edu>
2021-09-12 04:59:26 -0400

    FIX: retrieveRoute

--- scraper/routes/retrieveRoute.js
+++ scraper/routes/retrieveRoute.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const retrieveClass = require("../getClassInfo");
 
-router.get("/:target", (req, res) => {
+router.get("/:school/:target", (req, res) => {
+    const school = req.params.school;
     const target = req.params.target;
 
-    retrieveClass(target, req.query && req.query.dev === "true").then((info) => {
+    retrieveClass(school, target, req.query && req.query.dev === "true").then((info) => {
         return res.send(info);
     }).catch((e) => {
         return res.send(req.query && req.query.dev === "true" ? e : null);
