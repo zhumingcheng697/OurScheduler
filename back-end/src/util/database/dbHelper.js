24f5250d54bb33395c53480e2846df61ce4bbfc9
McCoy Zhu <mz1945@nyu.edu>
2021-11-28 23:50:38 -0500

    REMOVE: js

--- back-end/src/util/database/dbHelper.js
+++
@@ -1,5 +0,0 @@
-const { MongoClient } = require("mongodb");
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-module.exports = client.connect();
